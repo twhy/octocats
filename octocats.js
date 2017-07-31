07bda56fc2b32a098e12e631896571f4348da932
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-01 01:24:18 +0800

    Initial commit

---
+++ octocats.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+
+const URL = 'https://octodex.github.com';
+
+module.exports = function() {
+  request(URL, function(error, response, body) {
+    if (error) return console.log(error);
+
+    let $ = cheerio.load(body);
+    let images = [].map.call($('.preview-image > img'), function(image) {
+      return {
+        name: $(image).attr('alt'),
+        url: URL + $(image).data('src')
+      };
+    });
+
+    fs.writeFileSync('data.json', JSON.stringify(images));
+  });
+};
